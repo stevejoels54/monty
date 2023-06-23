f30d829cee0a775f3f5050560633b46670a576f6
joelsteven <joelofelectronics@gmail.com>
2023-06-23 03:56:06 +0300

    Added function description

--- pint.c
+++ pint.c
@@ -1,5 +1,12 @@
 #include "monty.h"
 
+/**
+ *pint - prints the value at the top of the stack
+ *@line_number: line numbers in ds
+ *@stack: double pointer to the head
+ *Return: nothing
+ */
+
 void pint(stack_t **stack, unsigned int line_number)
 {
 	if (*stack != NULL)
